235505cc6c12c46403576ad9dc606fd5979b5be0
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2026-04-17 14:23:12 +0530

    CaserCipher.java

--- CaserCipher.java
+++ CaserCipher.java
@@ -1,44 +1,51 @@
-import java.util.*; 
+import java.util.*;
 class CaserCipher
 {
 public static void main(String args[])
 {
 Scanner sc = new Scanner(System.in); 
-int shift; System.out.println("Enter text:"); text = sc.nextLine(); 
-System.out.println("Enter shift value:"); shift = sc.nextInt(); 
+String text,ctext="",dtext="";
+int shift;
+System.out.println("Enter text:");
+text = sc.nextLine(); 
+System.out.println("Enter shift value:");
+shift = sc.nextInt(); 
 for(int i=0;i<text.length();i++) 
 {
 char ch;
-ch = text.charAt(i);
-if(ch>='A'&&ch<='Z')
+ch = text.charAt(i); 
+if(ch>='A'&&ch<='Z') 
 {
 ctext = ctext+ (char)((ch-'A'+shift)%26+'A'); 
 }
 else if(ch>='a'&&ch<='z') 
+
 {
+ctext = ctext+ (char)((ch-'a'+shift)%26+'a'); }
 else
 {
-ctext = ctext+ (char)((ch-'a'+shift)%26+'a');	}
 ctext = ctext+ch;
 }
 }
 for(int i=0;i<ctext.length();i++) 
 {
 char ch;
-ch = ctext.charAt(i);
-if(ch>='A'&&ch<='Z') 
+ch = ctext.charAt(i); 
+if(ch>='A'&&ch<='Z')
 {
-dtext = dtext+ (char)((ch-'A'+26-shift)%26+'A');
+dtext = dtext+ (char)((ch-'A'+26-shift)%26+'A'); 
 }
 else if(ch>='a'&&ch<='z') 
 {
-dtext = dtext+ (char)((ch-'a'+26-shift)%26+'a');
+dtext = dtext+ (char)((ch-'a'+26-shift)%26+'a'); 
 }
 else
 {
 dtext = dtext+ch;
 }
 }
-System.out.println("Encrypted text :"+ctext); System.out.println("Decrypted text :"+dtext);
+
+System.out.println("Encrypted text :"+ctext);
+System.out.println("Decrypted text :"+dtext);
 }
 }
